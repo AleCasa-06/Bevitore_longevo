cde25867cb75d14c7bd86ab96844838a1dab16ca
CasarottoAlessandro <115339459+AleCasa-06@users.noreply.github.com>
2024-12-11 09:22:26 +0100

    Add files via upload

---
+++ telegramBot/src/main/java/BevitoreBot.java
@@ -0,0 +1,43 @@
+
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+public class BevitoreBot implements LongPollingSingleThreadUpdateConsumer {
+    private final TelegramClient telegramClient;
+
+    public BevitoreBot(String botToken) {
+        telegramClient = new OkHttpTelegramClient(botToken);
+    }
+
+
+    @Override
+    public void consume(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            if(update.getMessage().getText().equals("/start")){
+                message_text = "Benvenuto nel bot telegram più amato dai bevitori: controlla la lista comandi per capire cosa può fare questo bot!!!";
+            }
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = SendMessage // Create a message object
+                    .builder()
+                    .chatId(chat_id)
+                    .text(message_text)
+                    .build();
+
+
+            System.out.println(chat_id);
+            try {
+                telegramClient.execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
